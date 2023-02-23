bd8e622ba6c4e2b338759efeeb833ff7d202e850
Andrew Hawileh <andrewh829@gmail.com>
2023-02-23 14:00:00 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name)
+{
+    return drivers.filter((el) => el.toLowerCase() == name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name)
+{
+    return drivers.filter( (el) => el.charAt(0) == name.charAt(0))
+}
+
+
+function matchName(drivers, name)
+{
+    let matches = drivers.filter( (el) => el.name == name)
+    return matches;
+}
